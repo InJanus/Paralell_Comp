2fd0af9c8893833c38d63d8b5aa279ccfafb770e
Brian Culberson <briannculberson@gmail.com>
2021-11-09 11:41:36 -0500

    push

--- module4/ripple.cu
+++ module4/ripple.cu
@@ -31,12 +31,16 @@ __global__ void kernel( unsigned char *ptr, int ticks ) {
     float fx = x - DIM/2;
     float fy = y - DIM/2;
     float d = sqrtf( fx * fx + fy * fy );
-    unsigned char grey = (unsigned char)(128.0f + 127.0f *
-                                         cos(d/10.0f - ticks/7.0f) /
-                                         (d/10.0f + 1.0f));    
-    ptr[offset*4 + 0] = grey;
-    ptr[offset*4 + 1] = grey;
-    ptr[offset*4 + 2] = grey;
+    //unsigned char grey = (unsigned char)(128.0f + 127.0f *
+    //                                     cos(d/10.0f - ticks/7.0f) /
+    //                                     (d/10.0f + 1.0f));
+    //color code here
+    unsigned char red = (unsigned char)(128.0f+127.0f*cos(d/10.0f-ticks/7.0f)/(d/10.0f+1.0f));
+    unsigned char green = 0;//(unsigned char)(128.0f+127.0f*cos(d/10.0f-ticks/7.0f)/(d/10.0f+1.0f));
+    unsigned char blue = 0;//(unsigned char)(128.0f+127.0f*cos(d/10.0f-ticks/7.0f)/(d/10.0f+1.0f));  
+    ptr[offset*4 + 0] = red;
+    ptr[offset*4 + 1] = green;
+    ptr[offset*4 + 2] = blue;
     ptr[offset*4 + 3] = 255;
 }
 
