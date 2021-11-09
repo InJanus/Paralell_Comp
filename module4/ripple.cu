ac2e2504744d654022a4f5729af20efce3a12976
Brian Culberson <briannculberson@gmail.com>
2021-11-09 11:28:17 -0500

    psuh

--- module4/ripple.cu
+++ module4/ripple.cu
@@ -15,8 +15,8 @@
 
 
 #include "cuda.h"
-#include "../common/book.h"
-#include "../common/cpu_anim.h"
+#include "common/book.h"
+#include "common/cpu_anim.h"
 
 #define DIM 1024
 #define PI 3.1415926535897932f
