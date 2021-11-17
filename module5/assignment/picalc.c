fad120b589d1f84ff84b2eb510d816cbc4381745
InJanus <briannculberson@gmail.com>
2021-11-17 15:26:50 -0500

    push

--- module5/assignment/picalc.c
+++ module5/assignment/picalc.c
@@ -24,54 +24,43 @@ int main(int argc,char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
     MPI_Get_processor_name(processor_name,&namelen);
 
-    fprintf(stdout,"Process %d of %d on %s\n",
-	    myid, numprocs, processor_name);
+    fprintf(stdout,"Process %d of %d on %s\n", myid, numprocs, processor_name);
     
     int j = 1;
     n = 0;
-    //for (int j = 1; j <= 9; j++){
-        //n = pow(10,j);
-        //printf("interval %i or 10^%i\n", n, j);
-        while (!done){
-            if (myid == 0){
-
-                //printf("Enter the number of intervals: (0 quits) ");
-                //scanf("%d",&n);
-                //n = j;
-
-                //change the n over the course of 10 intervals
-
-                if (n==0){
-                    n=pow(10,j);
-                    j++;
-                }else{
-                    n=0;
-                }
-                startwtime = MPI_Wtime();
-            }
-            MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-            if (n == 0 && j==9){
-                done = 1;
+    while (!done){
+        if (myid == 0){
+            printf("%s", *argv);
+            if (n==0){
+                n=pow(10,j);
+                //j++;
             }else{
-                h   = 1.0 / (double) n;
-                sum = 0.0;
-                /* A slightly better approach starts from large i and works back */
-                for (i = myid + 1; i <= n; i += numprocs){
-                    x = h * ((double)i - 0.5);
-                    sum += f(x);
-                }
-                mypi = h * sum;
+                n=0;
+            }
+            startwtime = MPI_Wtime();
+        }
+        MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        if (n == 0){
+            done = 1;
+        }else{
+            h   = 1.0 / (double) n;
+            sum = 0.0;
+            /* A slightly better approach starts from large i and works back */
+            for (i = myid + 1; i <= n; i += numprocs){
+                x = h * ((double)i - 0.5);
+                sum += f(x);
+            }
+            mypi = h * sum;
 
-                MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-                if (myid == 0){
-                    printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
-                    endwtime = MPI_Wtime();
-                    printf("wall clock time = %f\n", endwtime-startwtime);	       
-                    fflush( stdout );
-                }
+            MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+            if (myid == 0){
+                printf("pi is approximately %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
+                endwtime = MPI_Wtime();
+                printf("wall clock time = %f\n", endwtime-startwtime);	       
+                fflush( stdout );
             }
         }
-    //}
+    }
     MPI_Finalize();
     return 0;
 }
